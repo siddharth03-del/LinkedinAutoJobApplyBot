7af0b010113544eb5b48cd979b21f5809233647f
siddharth03-del <siddharthsingh9361@gmail.com>
2025-02-10 14:26:42 +0530

    made some minor changes

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from Utils.SelectingJobs import SelectJobs
 from Utils.ChangePage import ChangePage
 from Utils.ClickJobApply import ClickJobApply
 from FillJobForm.FillJobFrom import FillJobForm
-driver.get('https://www.linkedin.com/jobs/search/?currentJobId=4053507858&f_AL=true&geoId=102713980&keywords=web%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true')
 
-PerformSignin(email='siddharthsingh9361@gmail.com', password='siddharth@123$')
+link = str(input("Please enter the link to your job page with easy apply applied in filter for more information you can refer to your Github account: "))
+email = str(input("Please enter your email address for LinkedIn account: "))
+password = str(input("Please enter your password for LinkedIn account:  "))
+driver.get(link)
+
+PerformSignin(email=email, password=password)
 
 SelectJobs()
